dc2377b69df6e62ff12a85bb95a988cd2d6b6d4e
Mateus Antônio Pilonetto <pilonettoantoniomateus@gmail.com>
2026-04-03 20:27:38 -0300

    Update index.html

--- projeto_gravity_store/main/modules/main.py
+++ projeto_gravity_store/main/modules/main.py
@@ -1,46 +1,51 @@
 from flask import Blueprint, render_template, request, session
-from database.conection import get_db_connection # IMPORT NOVO
+from database.conection import get_db_connection
 from bs4 import BeautifulSoup
 
-# Avisamos o Blueprint sobre as pastas relativas a ele
 main_bp = Blueprint('main', __name__, 
                     template_folder='../templates', 
                     static_folder='../static',
                     static_url_path='/main_static')
 
-# Rota trazida do app.py
 @main_bp.route("/")
 def home():
     userLogado = session.get('usuario_logado') 
     userDev = 0 
+    apps = [] # Lista vazia para guardar os apps
 
-    if userLogado:
-        conexao = None
-        cursor = None
-        try:
-            conexao = get_db_connection()
-            cursor = conexao.cursor()
+    conexao = None
+    cursor = None
+    try:
+        conexao = get_db_connection()
+        cursor = conexao.cursor()
 
-            # Alterado para is_dev e para o formato %s do MySQL
+        # 1. Verifica se o utilizador é Dev
+        if userLogado:
             comando_sql = "SELECT is_dev FROM people WHERE id = %s"
             cursor.execute(comando_sql, (userLogado,))
-
             resultado = cursor.fetchone()
-
             if resultado:
                 userDev = resultado[0]
 
-        except Exception as e:
-            # Imprimir o erro real ajuda caso dê problema de novo
-            return f"Erro interno no banco (main): {e}", 500
-            
-        finally:
-            if cursor: cursor.close()
-            if conexao: conexao.close()
-    
-    return render_template('index.html', admin_status=userDev)
+        # 2. NOVO: Vai buscar os últimos 10 aplicativos adicionados
+        comando_apps = "SELECT id, nome, dev_name, category, size_mb, icon_path FROM apps ORDER BY data_envio DESC LIMIT 10"
+        cursor.execute(comando_apps)
+        
+        # Converte o resultado para um formato de "Dicionário" (mais fácil para o HTML ler)
+        colunas = [desc[0] for desc in cursor.description]
+        apps_db = cursor.fetchall()
+        apps = [dict(zip(colunas, app)) for app in apps_db]
 
-# Rota trazida do app.py
+    except Exception as e:
+        return f"Erro interno na base de dados (main): {e}", 500
+        
+    finally:
+        if cursor: cursor.close()
+        if conexao: conexao.close()
+    
+    # Envia a lista 'apps' para a página HTML
+    return render_template('index.html', admin_status=userDev, apps=apps)
+
 @main_bp.route("/pesquisa")
 def pesquisa():
     termo = request.args.get('name') 
