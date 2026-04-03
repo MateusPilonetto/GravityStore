b092c9003568cda8efcb1edf26d285f88922e60f
Mateus Antônio Pilonetto <pilonettoantoniomateus@gmail.com>
2026-04-02 21:13:19 -0300

    Correção de bugs

--- projeto_gravity_store/main/static/script.js
+++ projeto_gravity_store/main/static/script.js
@@ -4,28 +4,20 @@
 const botao = document.getElementById("usuario");
 const menu = document.getElementById("discovered");
 
-// SÓ ADICIONA O EVENTO SE O BOTÃO EXISTIR (ou seja, se estiver logado)
 if (botao && menu) {
   botao.addEventListener("click", (evento) => {
-    evento.stopPropagation(); // Impede que o clique feche o menu imediatamente
+    evento.stopPropagation(); 
 
     if (menu.style.display === "block") {
       menu.style.display = "none";
     } else {
-      // Pega as coordenadas do mouse
-      const mouseX = evento.pageX;
-      const mouseY = evento.pageY;
-
-      // Move o menu para o local do clique e mostra
-      menu.style.left = mouseX + "px";
-      menu.style.top = mouseY + "px";
+      
       menu.style.display = "block";
     }
   });
 
-  // O código para fechar o menu clicando fora
+
   document.addEventListener("click", function (evento) {
-    // Se clicar em qualquer lugar que não seja o menu, ele fecha
     if (evento.target !== menu && !menu.contains(evento.target)) {
       menu.style.display = "none";
     }
@@ -39,7 +31,7 @@ console.log("--- DEBUG: Procurando o menu hambúrguer ---");
 const btnHamburguer = document.getElementById("btn-hamburguer");
 const listaMenu = document.getElementById("lista-menu");
 
-// Avisa se não encontrar os elementos
+
 if (!btnHamburguer)
   console.error("ERRO: O id 'btn-hamburguer' não foi encontrado!");
 if (!listaMenu) console.error("ERRO: O id 'lista-menu' não foi encontrado!");
